622f79b45b8fae5f86f5cd3280ee8702b18f1daf
unknown <xuantrangk54@gmail.com>
2025-10-29 20:37:03 +0700

    :tada: first commit

---
+++ src/test/java/com/supportkarateapi/supportkarateapi/SupportkarateapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supportkarateapi.supportkarateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportkarateapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
