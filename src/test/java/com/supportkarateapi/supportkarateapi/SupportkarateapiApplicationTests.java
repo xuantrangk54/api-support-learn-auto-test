1f84abaedb678942bf85bb4002c1243c81047629
unknown <xuantrangk54@gmail.com>
2025-10-30 05:12:40 +0700

    :ci: ci with genkins

--- src/test/java/com/supportkarateapi/supportkarateapi/SupportkarateapiApplicationTests.java
+++ src/test/java/com/supportkarateapi/supportkarateapi/SupportkarateapiApplicationTests.java
@@ -3,7 +3,7 @@ package com.supportkarateapi.supportkarateapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class SupportkarateapiApplicationTests {
 
 	@Test
