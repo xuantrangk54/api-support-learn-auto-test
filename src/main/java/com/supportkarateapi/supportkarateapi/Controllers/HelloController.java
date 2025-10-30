06a670a634d616cb7c64aed94fd131a165e68352
Trang <xuantrangk54@gmail.com>
2025-10-30 10:54:08 +0700

    try ci

--- src/main/java/com/supportkarateapi/supportkarateapi/Controllers/HelloController.java
+++ src/main/java/com/supportkarateapi/supportkarateapi/Controllers/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello() {
         return "Hello World!";
     }
+
+     @GetMapping("/trang")
+    public String hellotrang() {
+        return "Hello Worldtrang!";
+    }
 }
